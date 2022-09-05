3ae9db68bb4551f58549a868642f71ca9ad3de04
ronnifadhlillah <33772449+ronnifadhlillah@users.noreply.github.com>
2022-09-05 22:00:03 +0700

    Checkpoint#3
    Add default login required to be modified.

--- role/app.py
+++ role/app.py
@@ -20,16 +20,19 @@ def build():
     a=eng()
 
     @a.route('/',methods=['GET','POST'])
-    def index():
+    def login():
         if request.method=='POST':
             users=request.form['user']
             password=request.form['pass']
             if users in user and user[users]==password:
                 session['user']=users
+                return render_template('index.html')
             else:
                 print('false')
+        return render_template('login.html')
 
-
+    @a.route('/index',methods=['GET','POST'])
+    def index():
         return render_template('index.html')
 
     @a.before_app_request
@@ -41,6 +44,13 @@ def build():
             g.user=userName
     return a
 
+    def login_required(view):
+        @functools.wraps(view)
+        def wrapped_view(**kwargs):
+            if g.user is None:
+                return redirect(url_for('login'))
+            return view(**kwargs)
+        return wrapped_view
 
 if __name__=='__main__':
     a=build()
