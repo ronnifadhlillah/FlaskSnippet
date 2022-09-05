f0e64bca9ea798ac99fe515dc660ebb7b04952d2
ronnifadhlillah <33772449+ronnifadhlillah@users.noreply.github.com>
2022-09-05 22:49:35 +0700

    Checkpoint#4
    Foundataion revision for default middleware and session check.

--- role/app.py
+++ role/app.py
@@ -1,4 +1,4 @@
-from flask import Flask,render_template,Blueprint,request
+from flask import Flask,g,render_template,Blueprint,request,session
 import functools
 
 role={
@@ -25,32 +25,35 @@ def build():
             users=request.form['user']
             password=request.form['pass']
             if users in user and user[users]==password:
-                session['user']=users
+                session.clear()
+                session['user_id']=users
                 return render_template('index.html')
             else:
                 print('false')
         return render_template('login.html')
 
-    @a.route('/index',methods=['GET','POST'])
-    def index():
-        return render_template('index.html')
-
-    @a.before_app_request
-    def loadUser():
-        userName=session.get['user']
+    @a.before_request
+    def load_logged_in_user():
+        userName=session.get('user_id')
         if userName is None:
             g.user=None
         else:
             g.user=userName
-    return a
+
+    @a.route('/index',methods=['GET','POST'])
+    def index():
+        return render_template('index.html')
 
     def login_required(view):
+        print(view)
         @functools.wraps(view)
         def wrapped_view(**kwargs):
             if g.user is None:
                 return redirect(url_for('login'))
             return view(**kwargs)
         return wrapped_view
+
+    return a
 
 if __name__=='__main__':
     a=build()
